6d31182e78c3c6a90530f450044a59c77df6c323
balamurugan110799 <balamurugan99711@gmail.com>
2023-09-19 12:22:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import PopUp from './Popup';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [data, setData] = useState({
+    date: null,
+    name: ""
+  })
+  const [getAllDataJson, setAllDataJson] = useState()
+  const [startDate, setStartDate] = useState(new Date());
+  // console.log(myEpoch,"myEpoch")
+  const [popUpState, setPopUpState] = useState(false)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value
+    })
+  }
+  const epochConvertor = (date) => {
+    var myDate = new Date(date); // Your timezone!
+    var myEpoch = myDate.getTime() / 1000.0;
+    return myEpoch
+  }
+  const handleSubmit = (e,type) => {
+    e.preventDefault()
+     data.date = epochConvertor(startDate)
+     if(type==="update"){
+      console.log(data._id)
+      axios.put(`http://localhost:4000/api/updateDatePicker/${data._id}`, data)
+      .then((res) => {
+        console.log(res)
+        getAllData()
+        setPopUpState(false)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+     }else{
+      axios.post("http://localhost:4000/api/AddDatePicker", data)
+      .then((res) => {
+        console.log(res)
+        getAllData()
+        setPopUpState(false)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+     }
+   
+  }
+  const getAllData = () => {
+    axios.get("http://localhost:4000/api/getAllDatePicker")
+      .then((res) => {
+        res.data.data.forEach((v)=>{
+          let date=new Date(v.date*1000).toLocaleDateString()
+          v.showDate= date
+        })
+        console.log(res)
+        setAllDataJson(res.data.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  const handlePopup = () => {
+    setPopUpState(!popUpState)
+  }
+  const handleDelete = (v,i) =>{
+    console.log(v?._id)
+    axios.delete(`http://localhost:4000/api/deleteDatePicker/${v?._id}`)
+    .then((res) => {
+      console.log(res)
+      // setAllDataJson(res.data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+  const handleEdit = (v, i) => {
+    var myDate = new Date( v.date *1000);
+    setStartDate(myDate)
+    setData(v)
+    setPopUpState(true)
+  }
+
+  useEffect(() => {
+    getAllData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={() => handlePopup()}>Add</button>
+      <PopUp state={popUpState} handleClick={handlePopup} >
+        <form className='p-6'>
+          <input type="text" value={data.name} name="name" className='w-full border' onChange={(e) => handleChange(e)} />
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+          <button onClick={(e) => handleSubmit(e,"submit")}>Submit</button>
+          <button onClick={(e) => handleSubmit(e,"update")}>Update</button>
+        </form>
+      </PopUp>
+
+      <table>
+        <thead>
+          <tr>
+            <th>S.No</th>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {getAllDataJson?.map((v, i) => {
+            return (
+              <tr>
+                <td>{i}</td>
+                <td>{v?.showDate}</td>
+                <td>{v?.name}</td>
+                <td><button onClick={() => handleEdit(v, i)}>Edit</button></td>
+                <td><button onClick={() => handleDelete(v, i)}>Delete</button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/Popup.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+
+import { RiErrorWarningLine, RiCloseFill } from "react-icons/ri";
+import { VscWarning } from "react-icons/vsc";
+import { BiMessageCheck } from "react-icons/bi";
+import { FiInfo } from "react-icons/fi";
+export default function PopUp(props) {
+const {state, handleClick,name} =props
+console.log(props)
+  return (
+    <div>
+
+        {state ? <div >
+            <div>
+        <div className="justify-center bg-modal items-center scale-up-hor-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none sm:w-[100%] sm:px-3 " >
+          <div className="popup-box">
+            <div className={`${props.width === "sm" ? "w-[30%] " : "w-[30%]"}  ${props.width === "md" ? "w-[50%] " : ""} ${props.width === "lg" ? "w-[70%] " : ""} box   relative`}>
+              <div className={`  ${props.type == "info" ? "bg-[#3988d7]" : ""} ${props.type == "success" ? "bg-[#559b24]" : ""} ${props.type == "warning" ? "bg-[#c47c16]" : ""}
+              
+              ${props.type == "error" ? " bg-[#db484f]   " : ""}  bg-[#3988d7] py-2 px-4 heading  text-h4 flex justify-start `}>
+                {props.type == "error" ? <RiErrorWarningLine className="text-white  text-[26px] mt-1" /> : null}
+                {props.type == "warning" ? <VscWarning className="text-white  text-[26px] mt-1" /> : null}
+                {props.type == "success" ? <BiMessageCheck className="text-white  text-[26px] mt-1.5" /> : null}
+                {props.type == "info" ? <FiInfo className="text-white  text-[26px] mt-1" /> : null}
+                {props.type === undefined || props.type === null ? <FiInfo className="text-white  text-[26px] mt-1" /> : null }
+                <p className="text-white mx-3 text-h3 tracking-wide heading title pt-[1px]"> {props.title ?? props.type}</p>
+              </div>
+
+              {props.actionbar ? null :<div onClick={handleClick} className="absolute top-4 right-4 group rounded-md">
+                <div className={`  ${props.type == "success" ? " hover:bg-[#559b24]" : ""} ${props.type == "warning" ? " hover:bg-[#c47c16]" : ""} ${props.type == "error" ? " hover:bg-[#e33b44]" : ""} ${props.type == "info" ? "hover:bg-[#3988d7] " : ""} h-[20px] w-[20px] cursor-pointer duration-300  bg-[#fff] flex justify-center items-center  rounded-sm hover:bg-[#3988d7] `}>
+                  <RiCloseFill className="text-text-color group-hover:text-white" />
+                </div>
+              </div> }
+              
+
+              <div className={` ${props.width === "sm" ? "h-[30vh] overflow-auto " : ""} ${props.width === "md" ? "h-[50vh] overflow-auto " : ""} ${props.width === "lg" ? " overflow-auto" : ""}  `}>
+                <div>
+
+                {/* <p className="w-[80%]  mx-auto text-text-color pt-6 text-center text-base pb-6"> {props.message}</p> */}
+                {props.children}
+                {props.actionbar ? <div className=" flex justify-end px-2 pb-2">
+                <button    onClick={handleClick} className="bg-dashboard cursor-pointer py-1  text-tiny hover:bg-dashboard-hover px-8 text-white">
+                    Cancel
+                  </button>
+                  <button className="bg-dashboard cursor-pointer mx-2 text-tiny py-1 hover:bg-dashboard-hover px-8 text-white">
+                  Save
+                  </button>
+               
+                  </div>:null}
+                <div></div>
+                </div>
+             
+
+              </div>
+
+
+         
+            </div>
+          </div>
+        </div>
+      </div> 
+        </div> : null}
+        
+    </div>
+  )
+}
